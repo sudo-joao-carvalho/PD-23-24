67793d4a218aa8a63796211e6bc55e58ecde8a3c
sudo-john-blossom <73914719+sudo-joao-carvalho@users.noreply.github.com>
2023-11-25 23:28:47 +0000

    Update DBManager.java

--- pd_23_24/src/resources/db/DBManager.java
+++ pd_23_24/src/resources/db/DBManager.java
@@ -25,9 +25,9 @@ public class DBManager {
     }
 
     public boolean connectToDB(String directory, int port) {
-        if (new File(directory + "/PD-2023-24-TP.db" /*"/PD-2023-24-TP-" + port + ".db"*/).exists()) {
+        if (new File(directory + "/PD-2023-24-TP.db").exists()) {
             try {
-                this.conn = DriverManager.getConnection("jdbc:sqlite:" + directory + "/PD-2023-24-TP.db"/*"/PD-2022-23-TP-" + port + ".db\""*/);
+                this.conn = DriverManager.getConnection("jdbc:sqlite:" + directory + "/PD-2023-24-TP.db");
             } catch(SQLException e) {
                 e.printStackTrace();
                 return false;
